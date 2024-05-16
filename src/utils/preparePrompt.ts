06f19f340cc8ff38027ecabfc395fa7bce2f23d9
Colin McNeil <colin.mcneil@docker.com>
2024-05-16 11:51:36 -0400

    Add configurable prompt generation image

--- src/utils/preparePrompt.ts
+++ src/utils/preparePrompt.ts
@@ -1,11 +1,14 @@
 import { execSync, spawnSync } from "child_process";
+import { workspace } from "vscode";
 
 export const prepareProjectPrompt = (facts: { [key: string]: any }, username: string) => {
 
     const platform = process.platform;
 
+    const promptImage = workspace.getConfiguration('docker.make-runbook').get('prompt-image') as string;
+
     // Use JQ to pass facts and platform to the prepareRunbookPrompt.js script
-    const result = spawnSync('docker', ['run', 'vonwig/prompts', 'prepareRunbookPrompt.js', JSON.stringify(facts), username, JSON.stringify(platform)]);
+    const result = spawnSync('docker', ['run', promptImage, 'prepareRunbookPrompt.js', JSON.stringify(facts), username, JSON.stringify(platform)]);
 
     if (result.error) {
         throw result.error;
