ef548e75fb40fa9bcccf4f1d7bd4e2a18e3f589b
Jim Clark <slimslenderslacks@gmail.com>
2024-06-10 12:58:25 -0700

    Cache prompt repo clones in a volume
    Use a volume mount to cache the prompt repos between clones

--- src/utils/preparePrompt.ts
+++ src/utils/preparePrompt.ts
@@ -18,7 +18,7 @@ export const prepareProjectPrompt = (projectRoot: vscode.WorkspaceFolder, userna
 
     const promptImage = vscode.workspace.getConfiguration('docker.make-runbook').get('prompt-image') as string;
 
-    const result = spawnSync('docker', ['run', '--rm', "-v", "/var/run/docker.sock:/var/run/docker.sock", promptImage, projectRoot.uri.fsPath, username, platform, promptType]);
+    const result = spawnSync('docker', ['run', '--rm', "-v", "/var/run/docker.sock:/var/run/docker.sock", "--mount", "type=volume,source=docker-prompts,target=/prompts", promptImage, projectRoot.uri.fsPath, username, platform, promptType]);
 
     if (result.error) {
         throw result.error;
